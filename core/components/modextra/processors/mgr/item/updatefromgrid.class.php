3952b008a7008f2c9e994dbd9577dc52d991b4a9
Брежнев Иван <brezhnev.ivan@yahoo.com>
2014-03-31 22:23:20 +0300

    Update and rename updateFromGrid.class.php to updatefromgrid.class.php

--- core/components/modextra/processors/mgr/item/updatefromgrid.class.php
+++ core/components/modextra/processors/mgr/item/updatefromgrid.class.php
@@ -1,13 +1,11 @@
 <?php
-require_once dirname(__FILE__) . '/update.class.php';
-/**
- * Update an item from a grid
- *
- * @package modExtra
- * @subpackage processors.mgr.item
- */
+
+require_once 'update.class.php';
+
 class modExtraItemUpdateFromGridProcessor extends modExtraItemUpdateProcessor {
-    public function initialize() {
+
+    public function initialize()
+    {
         $data = $this->getProperty('data');
         if (empty($data)) return $this->modx->lexicon('invalid_data');
         $properties = $this->modx->fromJSON($data);
@@ -17,4 +15,5 @@ class modExtraItemUpdateFromGridProcessor extends modExtraItemUpdateProcessor {
         return parent::initialize();
     }
 }
+
 return 'modExtraItemUpdateFromGridProcessor';
